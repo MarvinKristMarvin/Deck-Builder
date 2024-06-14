ce9e8a18109929b0768da2cc7cfa60ec79a79641
MarvinKristMarvin <marvin.krist@oclock.school>
2024-06-14 14:31:35 +0200

    etape 1 fini

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,18 +1,36 @@
-const dataMapper = require('../dataMapper.js');
+const dataMapper = require("../dataMapper.js");
 
 const mainController = {
   homePage: async (req, res) => {
     try {
       const cards = await dataMapper.getAllCards();
-      res.render('cardList', {
+      res.render("cardList", {
         cards,
-        title: 'Liste des cartes'
+        title: "Liste des cartes",
       });
     } catch (error) {
       console.error(error);
-      res.status(500).send(`An error occured with the database :\n${error.message}`);
+      res
+        .status(500)
+        .send(`An error occured with the database :\n${error.message}`);
     }
-  }
+  },
+
+  cardPage: async (req, res) => {
+    try {
+      const cardId = Number(req.params.id);
+      const card = await dataMapper.getCard(cardId);
+      res.render("cardInfo", {
+        card,
+        title: "Informations sur la carte",
+      });
+    } catch (error) {
+      console.error(error);
+      res
+        .status(500)
+        .send(`An error occured with the database :\n${error.message}`);
+    }
+  },
 };
 
 module.exports = mainController;
--- app/dataMapper.js
+++ app/dataMapper.js
@@ -1,12 +1,18 @@
-const database = require('./database');
+const database = require("./database");
 
 const dataMapper = {
   async getAllCards() {
     const query = "SELECT * FROM card";
     const result = await database.query(query);
     return result.rows;
-  }
-};
+  },
 
+  async getCard(cardId) {
+    const query = `SELECT * FROM card WHERE id = ${cardId}`;
+    const result = await database.query(query);
+    console.log(result.rows[0]);
+    return result.rows[0];
+  },
+};
 
 module.exports = dataMapper;
